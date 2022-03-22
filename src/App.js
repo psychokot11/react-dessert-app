172162fd36715cc4edf84d37e377a36bc788127a
psychokot11 <z.stawarska@gmail.com>
2022-03-23 00:35:52 +0100

    Imported { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return <div className="App">app</div>;
 }
--- src/components/desserts/NewDessertForm.js
+++ src/components/desserts/NewDessertForm.js
@@ -1,7 +1,7 @@
 import classes from "./NewDessertForm.module.css";
 
 function NewDessertForm() {
-  return <div className="NewDessertForm"></div>;
+  return <div className="NewDessertForm">new dessert form</div>;
 }
 
 export default NewDessertForm;
