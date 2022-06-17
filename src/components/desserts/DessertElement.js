7bbc64d15101594dde7140fdb1050c3ee4fa1d9d
psychokot11 <z.stawarska@gmail.com>
2022-06-17 23:33:20 +0200

    Add font awesome svg and onClick function

--- src/components/desserts/DessertElement.js
+++ src/components/desserts/DessertElement.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import "./DessertElement.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 function DessertElement(props) {
+  const [isStarClicked, setIsStarClicked] = useState(false);
+  const [starredClass, setStarredClass] = useState("not-starred");
+  const handleFavourites = () => {
+    if (!isStarClicked) {
+      setIsStarClicked(true);
+      setStarredClass("starred");
+    } else {
+      setIsStarClicked(false);
+      setStarredClass("not-starred");
+    }
+  };
+
   return (
     <div className="DessertElement">
       <Card className="card" style={{ width: "22rem" }}>
@@ -12,6 +27,11 @@ function DessertElement(props) {
           <a href={props.recipe} target="_blank" rel="noreferrer">
             <Button className="button">Go to recipe</Button>
           </a>
+          <FontAwesomeIcon
+            icon={faStar}
+            className={starredClass}
+            onClick={handleFavourites}
+          />
         </Card.Body>
       </Card>
     </div>
