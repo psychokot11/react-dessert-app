045a137f68b8b8c7f19d3ba9c0ad0201dfcdb424
psychokot11 <z.stawarska@gmail.com>
2022-06-18 18:14:30 +0200

    Add functionality to display starred items

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,3 +1,14 @@
+import { useContext } from "react";
+import StarredContext from "../context/starred-context";
+import DessertList from "../components/desserts/DessertsList";
+
 export default function Starred() {
-  return <div className="Starred">hello from favorities</div>;
+  const starredCtx = useContext(StarredContext);
+  let content;
+  if (starredCtx.totalStarred === 0) {
+    content = <p className="starred-text">No Starred Items</p>;
+  } else {
+    content = <DessertList desserts={starredCtx.starred} />;
+  }
+  return <div className="Starred">{content}</div>;
 }
